68363fd04e9c0381ec0f6df97b60c68d2d062ae8
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-29 22:55:26 +0530

    urls file and views

--- authapp/views.py
+++ authapp/views.py
@@ -1,6 +1,12 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
+
 def register(request):
-    return render(request,'register.html')
+    return render(request, 'register.html')
+
+
+def login(request):
+    return render(request, 'home.html')
