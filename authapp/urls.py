a2a31162066d5e8a7b8dcab25da88ba196db5e4b
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-29 22:58:18 +0530

    urls file and views

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import register
+from .views import login, register
 
 urlpatterns = [
-    path('/register',register, name="register"),
-    path('/login',login, name="login")
+    path('register', register, name="register"),
+    path('login', login, name="login"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('dashboard.urls')),
+    path('', include('authapp.urls')),
     path('admin/', admin.site.urls),
 ]
 
