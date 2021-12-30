706e765d2d69ae35e9e594b1e1c0ceb3f81154fb
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-30 23:33:57 +0530

    created services modal

--- services/models.py
+++ services/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=400)
+    active = models.BooleanField(default=False)
+
+
+class ServicesModel(models.Model):
+    category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=400)
+    min_order = models.IntegerField()
+    max_order = models.IntegerField()
+    rate = models.IntegerField(help_text="Per 1000 ")
+    average_time_hours = models.IntegerField(blank=True, null=True)
+    average_time_minutes = models.IntegerField(blank=True, null=True)
+    description = models.TextField(max_length=4000, blank=True, null=True)
+    service_id = models.IntegerField()
+    active = models.BooleanField(default=False)
