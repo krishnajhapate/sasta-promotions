04266b95cac8e08b96a553cc8894e69f29856bee
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-30 22:43:24 +0530

    added about us page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from dashboard.views import home_page
+from dashboard.views import about_page, home_page
 
 urlpatterns = [
     path('', home_page, name="home"),
+    path('about', about_page, name="about"),
 ]
