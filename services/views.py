0abc85216100fc84ce37374823fdbee9bff55dcc
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-31 11:18:43 +0530

    services page working

--- services/views.py
+++ services/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+from services.models import CategoryModel, ServicesModel
+
 # Create your views here.
 
 
 def services(request):
-    return render(request, 'services.html')
+    categories = CategoryModel.objects.filter(active=True)
+    services = []
+    for category in categories:
+        service = ServicesModel.objects.filter(active=True, category=category)
+        if service.count() > 0:
+            category.services = service
+            services.append(category)
+    # print(categories)
+    return render(request, 'services.html', {"categories": services})
