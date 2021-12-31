778b3c2c82ac86d5c8b14c18e2c9deb311a1bdb6
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-31 08:51:48 +0530

    updated service modals

--- services/models.py
+++ services/models.py
@@ -22,7 +22,7 @@ class ServicesModel(models.Model):
     average_time_hours = models.IntegerField(blank=True, null=True)
     average_time_minutes = models.IntegerField(blank=True, null=True)
     description = models.TextField(max_length=4000, blank=True, null=True)
-    service_id = models.IntegerField()
+    service_id = models.IntegerField(blank=True, null=True)
     active = models.BooleanField(default=False)
 
     def __str__(self) -> str:
