fa4fd7c80fe8b734380f087f536b554f35a07d2e
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-02 17:36:47 +0530

    addedauth module

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    
+    name = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=100, blank=True)
+ 
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name']
+
+    # objects = CustomUserManager()
+
+    def __str__(self):
+        return str(self.name)
+
