309dc871a60914585cbfbd8e889b79004d994091
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-02 23:36:50 +0530

    updated auth flow

--- authapp/views.py
+++ authapp/views.py
@@ -19,11 +19,35 @@ def register(request):
 
         user = User.objects.filter(email=email)
         if user.count() > 0:
-            return render(request, 'register.html', {"email_error": True})
+            return render(
+                request, 'register.html', {
+                    "email_error": True,
+                    "email": email,
+                    "first_name": firstname,
+                    "last_name": lastname,
+                    "phone": phone
+                })
 
         user = User.objects.filter(username=username)
         if user.count() > 0:
-            return render(request, 'register.html', {"username_error": True})
+            return render(
+                request, 'register.html', {
+                    "username_error": True,
+                    "email": email,
+                    "first_name": firstname,
+                    "last_name": lastname,
+                    "phone": phone,
+                })
+        if password1 != password2:
+            return render(
+                request, 'register.html', {
+                    "password_error": True,
+                    "email": email,
+                    "first_name": firstname,
+                    "last_name": lastname,
+                    'username': username,
+                    "phone": phone
+                })
 
         user_create = User.objects.create(
             username=username,
