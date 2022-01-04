08d6708903c5ac4a55a299946a7487cc86697458
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-04 20:52:53 +0530

    added static

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
-# from django.contr
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required 
 # Create your views here.
 
 
 def home_page(request):
+    if request.user:
+        return redirect('dashboard')
     return render(request, "home.html")
 
-
+@login_required
 def dashboard(request):
     return render(request, "dashboard.html")
 
