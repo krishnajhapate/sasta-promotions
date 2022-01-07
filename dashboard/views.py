56151378a91a6d4c4dae4cc88eb1acb22033f0da
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-07 23:32:24 +0530

    orders create working

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from services.models import CategoryModel, ServicesModel
+from orders.models import OrdersModel
 # Create your views here.
 
 
@@ -21,8 +22,18 @@ def home_page(request):
 def dashboard(request):
 
     if request.method == "POST":
-        
-        print(request.POST)
+        service_id = request.POST.get('service', None)
+        link = request.POST.get('link', None)
+        quantity = request.POST.get('quantity', None)
+        service = ServicesModel.objects.get(id=service_id)
+        charge = (service.rate * float(quantity)) / 1000
+
+        order_create = OrdersModel.objects.create(service=service,
+                                                  quantity=quantity,
+                                                  link=link,
+                                                  charge=charge)
+        if order_create:
+            return redirect('orders')
 
 
     categories = CategoryModel.objects.filter(active=True)
