9efa3d51279aa79032c627f0410842fafabad517
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-07 09:08:40 +0530

    update

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from services.models import CategoryModel, ServicesModel
 # Create your views here.
 
 
 def home_page(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
+    categories = CategoryModel.objects.filter(active=True)
 
-    return render(request, "home.html")
+    services = []
+    for category in categories:
+        service = ServicesModel.objects.filter(active=True, category=category)
+        if service.count() > 0:
+            category.services = service
+    return render(request, "home.html", {"category": services})
 
 
 @login_required
 def dashboard(request):
-    return render(request, "dashboard.html")
+    categories = CategoryModel.objects.filter(active=True)
+
+    print(categories)
+    services = []
+    for category in categories:
+        service = ServicesModel.objects.filter(active=True, category=category)
+        if service.count() > 0:
+            category.services = service
+            services.append(category)
+    return render(request, "dashboard.html", {"category": categories})
 
 
 def about_page(request):
