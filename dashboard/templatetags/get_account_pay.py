b2ae8d35953ef41705c602a48f5031b25a1db281
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-08 20:49:10 +0530

    account balance added

--- dashboard/templatetags/get_account_pay.py
+++ dashboard/templatetags/get_account_pay.py
@@ -1,6 +1,7 @@
 from django import template
 
 from authapp.models import AccountBalance, User
+from orders.models import OrdersModel
 
 register = template.Library()
 
@@ -8,3 +9,12 @@ register = template.Library()
 @register.simple_tag
 def get_account_balance(user):
     return AccountBalance.objects.get(user=user).money
+
+
+@register.simple_tag
+def get_spent_balance(user):
+    balance = OrdersModel.objects.filter(user=user).exclude(status="Cancelled")
+    total = 0
+    for i in balance:
+        total += balance.charge
+    return total
