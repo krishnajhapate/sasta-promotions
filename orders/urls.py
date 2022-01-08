1bc7fa67775ad2ad315a5a684d1e2e1032a6a8bb
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-08 12:18:40 +0530

    order searching working

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,5 @@ from .views import orders
 
 urlpatterns = [
     path('orders', orders, name="orders"),
+    path('orders/<str:status>', orders, name="order_status"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,11 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required
-def orders(request):
+def orders(request, status=None):
     orders = OrdersModel.objects.filter(user=request.user)
-    return render(request, "orders.html", {"orders": orders})
+    if status:
+        orders = orders.filter(status=status)
+    search = request.GET.get('search', None)
+    if search:
+        orders = orders.filter(link__contains=search)
+    return render(request, "orders.html", {"orders": orders,"search":search or ""})
