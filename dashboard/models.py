1c97261b7bf4a67286b80bce77ed7591da7ac2b4
krishnajhapate <ki54.jhapate@gmail.com>
2022-02-10 16:58:33 +0530

    whatsapp button support added

--- dashboard/models.py
+++ dashboard/models.py
@@ -13,6 +13,7 @@ class Settings(models.Model):
     sasta_id = models.CharField(max_length=300, blank=True, null=True)
     sasta_active = models.BooleanField(default=False)
     site_headline = models.CharField(max_length=500, blank=True, null=True)
+    whatsapp_no = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
         verbose_name = "Setting"
