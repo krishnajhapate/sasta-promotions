0add3e4bf364aeb26c0d0b348ea77925c66e87f3
krishnajhapate <ki54.jhapate@gmail.com>
2022-02-28 11:38:56 +0530

    issue fix

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,42 +1,8 @@
 from authapp.models import AccountBalance
 from orders.models import OrdersModel
-import requests
+from django.shortcuts import render
 
 from services.models import CategoryModel, Offers, ServicesModel
-
-
-def place_order(request):
-    offers = Offers.objects.filter(user=request.user)
-    service_id = request.POST.get('service', None)
-    link = request.POST.get('link', None)
-    quantity = request.POST.get('quantity', None)
-    service = ServicesModel.objects.get(id=service_id)
-    offers = offers.filter(service=service)
-    if offers.exists():
-        service = offers.first().service
-        service.rate = offers.first().price
-    print(service, service.rate)
-    charge = (service.rate * float(quantity)) / 1000
-    order_create = OrdersModel.objects.create(
-        service=service,
-        quantity=quantity,
-        link=link,
-        charge=charge,
-        user=request.user,
-    )
-    # if service.api and service.api.active and service.service_id:
-    #     api_url = service.api.api_url + f"?key={service.api.api_key}&service={service.service_id}&action=add&link={order_create.link}&quantity={order_create.quantity}"
-    #     res = requests.post(api_url, params=request.GET)
-    #     try:
-    #         if res.json()['order']:
-    #             order_create.status = "Processing"
-    #             order_create.third_party_id = res.json()['order']
-    #             order_create.third_party_name = 'sasta'
-    #             order_create.save()
-    #     except:
-    #         pass
-
-    return order_create
 
 
 def get_cat(request):
@@ -64,3 +30,42 @@ def get_cat(request):
             services.append(category)
 
     return services
+
+
+def place_order(request):
+    offers = Offers.objects.filter(user=request.user)
+    service_id = request.POST.get('service', None)
+    link = request.POST.get('link', None)
+    quantity = request.POST.get('quantity', None)
+    service = ServicesModel.objects.get(id=service_id)
+    offers = offers.filter(service=service)
+    if offers.exists():
+        service = offers.first().service
+        service.rate = offers.first().price
+    print(service, service.rate)
+    charge = (service.rate * float(quantity)) / 1000
+    account_balance = AccountBalance.objects.get(user=request.user)
+
+    if charge > account_balance.money:
+        error_message = "No sufficient account balance to place this order"
+        return error_message, False
+    order_create = OrdersModel.objects.create(
+        service=service,
+        quantity=quantity,
+        link=link,
+        charge=charge,
+        user=request.user,
+    )
+    # if service.api and service.api.active and service.service_id:
+    #     api_url = service.api.api_url + f"?key={service.api.api_key}&service={service.service_id}&action=add&link={order_create.link}&quantity={order_create.quantity}"
+    #     res = requests.post(api_url, params=request.GET)
+    #     try:
+    #         if res.json()['order']:
+    #             order_create.status = "Processing"
+    #             order_create.third_party_id = res.json()['order']
+    #             order_create.third_party_name = 'sasta'
+    #             order_create.save()
+    #     except:
+    #         pass
+
+    return order_create, True
