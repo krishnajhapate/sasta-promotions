d3e3e67301406ac9eb02b87a508bdac13dc6651c
krishnajhapate <ki54.jhapate@gmail.com>
2022-03-22 00:12:22 +0530

    price issue fox

--- services/utils.py
+++ services/utils.py
@@ -11,14 +11,24 @@ def get_ser(request):
 
     services = []
     if offer_services:
-        for ser in offer_services:
-            print(ser.service, ser.service.rate, ser.offer_type)
-            if ser.offer_type == "Percentage":
-                ser.service.rate = ser.service.rate - (ser.service.rate *
-                                                       ser.price) / 100
-                services.append(ser.service)
-            else:
-                services.append(ser.service)
+        print(offer_services)
+        if offer_services.filter(offer_type="Percentage",
+                                 service__isnull=True).exists():
+            offer = offer_services.filter(offer_type="Percentage",
+                                          service__isnull=True).first()
+
+            for ser in service_main:
+                ser.rate = ser.rate - (ser.rate * offer.price) / 100
+        else:
+            for ser in offer_services:
+                # print(ser.service, ser.service.rate, ser.offer_type)
+                if ser.service and ser.offer_type == "Percentage":
+                    ser.service.rate = ser.service.rate - (ser.service.rate *
+                                                           ser.price) / 100
+                    services.append(ser.service)
+                else:
+                    ser.service.rate = ser.price
+                    services.append(ser.service)
 
     for ser in service_main:
         if ser.id not in [x.id for x in services]:
