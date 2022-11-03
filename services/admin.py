b6c0d524bbee124e37f1baafc118aec98213aa2c
krishnajhapate <ki54.jhapate@gmail.com>
2022-11-03 23:54:20 +0530

    services update

--- services/admin.py
+++ services/admin.py
@@ -14,6 +14,15 @@ class ServiceModelAdmin(admin.StackedInline):
     list_display = ('id', )
 
 
+
+@admin.action(description='Mark selected as activate')
+def mark_active_services(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+@admin.action(description='Mark selected as deactivate')
+def mark_inactive_services(modeladmin, request, queryset):
+    queryset.update(active=False)
+
 class ServiceModelAdminShow(admin.ModelAdmin):
     list_display = (
         'id',
@@ -22,15 +31,22 @@ class ServiceModelAdminShow(admin.ModelAdmin):
         'rate',
         'api',
         'ranking',
+        'refill',
         'service_id',
+        'active'
     )
     ordering = ['ranking']
 
-    search_fields = ('id', 'name', 'category__name', 'api__name', 'service_id')
+    search_fields = ('id', 'name', 'category', 'api__name', 'service_id',)
     list_filter = (
         'active',
         'category',
     )
+    autocomplete_fields = ('category',)
+    actions = [mark_active_services, mark_inactive_services]
+
+    def category_name(self, obj):
+        return obj.category.name
 
 
 @admin.register(Offers)
@@ -42,18 +58,23 @@ class OfferAdmin(admin.ModelAdmin):
         'price',
     )
     search_fields = ('user', )
+    autocomplete_fields = ('user', 'service')
 
+
+
+@admin.action(description='Mark selected as activate')
+def mark_active_category(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+@admin.action(description='Mark selected as deactivate')
+def mark_inactive_category(modeladmin, request, queryset):
+    queryset.update(active=False)
 
 class CategoryModelAdmin(admin.ModelAdmin):
-    # add_form = ServiceAddForm
-
-    # add_fieldsets = ((None, {
-    #     'classes': ('wide', ),
-    #     'fields': ('name', 'active', 'url'),
-    # }), )
-
     list_display = ('id', 'name', 'ranking', 'active')
+    search_fields = ('id', 'name')
     list_filter = ('active', )
+    actions = [mark_active_category, mark_inactive_category]
 
 
 class MessagesAdmin(admin.TabularInline):
