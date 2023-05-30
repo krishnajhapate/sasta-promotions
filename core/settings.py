04d451616a5d2d7c30a597ca4f75b4ea2c4cb612
krishna <ki54.jhapate@gmail.com>
2023-05-30 14:57:53 +0530

    updated config

--- core/settings.py
+++ core/settings.py
@@ -92,15 +92,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if debug:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-else:
-    DATABASES = {
+# if debug:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# else:
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': env('DB_NAME'),
